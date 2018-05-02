2c456767e31f980e040f2edac3abb99fc9ff19f0
ShiftedCoding <kalebr82@outlook.com>
2018-05-02 15:14:27 -0400

    Updating LoginHandler.js

--- LoginHandler.js
+++ LoginHandler.js
@@ -1,12 +1,12 @@
+var config = {
+  apiKey: "AIzaSyBf3P8gXOJG-msYlyZHbKssXuRUv5ykeGY",
+  authDomain: "shiftedmc-forums.firebaseapp.com",
+  databaseURL: "https://shiftedmc-forums.firebaseio.com",
+  projectId: "shiftedmc-forums",
+  storageBucket: "shiftedmc-forums.appspot.com",
+  messagingSenderId: "660353953720"
+};
 function login() {
-    var config = {
-      apiKey: "AIzaSyBf3P8gXOJG-msYlyZHbKssXuRUv5ykeGY",
-      authDomain: "shiftedmc-forums.firebaseapp.com",
-      databaseURL: "https://shiftedmc-forums.firebaseio.com",
-      projectId: "shiftedmc-forums",
-      storageBucket: "shiftedmc-forums.appspot.com",
-      messagingSenderId: "660353953720"
-    };
     firebase.initializeApp(config);
     firebase.auth().signInWithEmailAndPassword(document.getElementById("email").value, document.getElementById("password").value).catch(function(error) {
         console.log("Error code " + error.code + ": " + error.message);
